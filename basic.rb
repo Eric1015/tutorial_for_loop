6bbf8f690d679d7907685ee3bb4f1522aeebd0a9
love1 <love1dbsb77@hotmail.com>
2018-10-21 23:08:13 -0700

    Added Japanese comments for people who are gonna learn for loop from here

--- basic.rb
+++ basic.rb
@@ -1,3 +1,5 @@
+# まずは基本のfor loop
+# この例ですと、iは1から5まで一回のループの最後に1ずつ増えます
 def printOut1()
     for i in 1..5
         print i
@@ -5,6 +7,9 @@ def printOut1()
     end
 end
 
+# 変わりまして、点が３つに増えると、1から4までつまり、
+# 最後の数字を含まなくなります。
+# なので、点の数には気をつけましょう
 def printOut2()
     for i in 1...5
         print i
--- matrix.rb
+++ matrix.rb
@@ -1,6 +1,13 @@
+# 少しややこしくなるかもしれませんが、
+# このように配列の中に別の配列を入れることができます。
+# 形的には行列に似ています。
+# この二重の配列のそれぞれの要素を一つずつ見ていきたいのでしたら、
+# 外側の配列に対するループと内側に対するループの両方が必要になってきます。
 def printOutMatrix(matrix)
     for i in 0...matrix.length
+        # matrix[0]: [1,2,3], matrix[1]: [4,5,6]
         for j in 0...matrix[0].length
+            # matrix[0][0]: 1, matrix[0][1]: 2
             print matrix[i][j]
             print " "
         end
